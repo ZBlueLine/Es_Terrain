e92313dbaba7a171bd5068c5bb7761e879e3441e
zblueline <zblueline@163.com>
2021-01-08 15:30:25 +0800

    添加了撤销功能

---
+++ Assets/T4M/Scripts/Util.cs
@@ -0,0 +1,11 @@
+﻿namespace UtilStruct
+{
+	using UnityEngine;
+	public struct m_UndoNode
+	{
+		public Color[] Color1;
+		public Color[] Color2;
+		public Color[] Color3;
+		public Color[] Color4;
+	};
+}
